5ed80ff516471d8504e804e310c7766a7673289c
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-12-08 11:32:17 -0600

    completed excercise00a

--- week-01/exercises/variables-and-types-exercises/src/Exercise00a.java
+++ week-01/exercises/variables-and-types-exercises/src/Exercise00a.java
@@ -2,8 +2,16 @@ public class Exercise00a {
     public static void main(String[] args) {
         // 1. Add a single line comment below. Hint: this instruction is a single line comment.
 
+        // Here lies a single line comment
+
         /* 2. Add a multi-line comment below.
         Hint: this instruction is a multi-line comment.
          */
+
+        /*
+        Here lies a multi-line
+        comment!
+         */
+
     }
 }
