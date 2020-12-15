a6addd8e9f7c650d1b73a326d7dd337becdf7b6c
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-12-15 12:13:10 -0600

    fixed issue

--- week-02/exercises/object-exercises/src/Exercise05.java
+++ week-02/exercises/object-exercises/src/Exercise05.java
@@ -4,22 +4,25 @@ public class Exercise05 {
 
     public static void main(String[] args) {
         // STILL NEEDS WORK
-//        Scanner console = new Scanner(System.in);
-//
-//        Musician[] musicians = new Musician[5];
-//
-//        for (int i = 0; i < musicians.length; i++) {
-//            Musician m = new Musician();
-//            System.out.print("Musician name: ");
-//            m.setName(console.nextLine());
-//            System.out.print("Musician rating: ");
-//            int rating = Integer.parseInt(console.nextLine());
-//            m.setRating(rating);
-//            musicians[i].getName() = new Musician(m.getName());
-//            System.out.println(musicians);
-//        }
+        Scanner console = new Scanner(System.in);
 
+        Musician[] musicians = new Musician[5];
 
+        for (int i = 0; i < musicians.length; i++) {
+            Musician musician = new Musician();
+            System.out.print("Musician name: ");
+            musician.setName(console.nextLine());
+            System.out.print("Musician rating: ");
+            int rating = Integer.parseInt(console.nextLine());
+            musician.setRating(rating);
+            musicians[i] = new Musician(musician.getName(), musician.getRating());
+
+        }
+
+        for (Musician m : musicians){
+            System.out.printf("%s: %s%n", m.getName(), m.getRating());
+
+        }
 
         // 1. Use a loop to populate the `musicians` array with your top 5 favorite musicians.
         // (Replace Frank Ocean.)
