9801cdf3531cca1d3c5b47ac6fc7a8cc7ae0b605
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2021-02-06 13:28:04 -0800

    can add & update

--- week-08/assessment/field-agent/src/components/Header.js
+++ week-08/assessment/field-agent/src/components/Header.js
@@ -1,25 +1,25 @@
 import '../assets/css/Header.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <ul id = 'header' class="nav justify-content-center">
-            <Link to="/agents" className={window.location.pathname === "/agents"}>
-                <li class="nav-item">
+        <ul id='header' className="nav justify-content-center">
+            <li className="nav-item">
+                <Link to="/agents">
                     <a className="nav-link" href="/agents">Agents</a>
-                </li>
-            </Link>
-            <li class="nav-item">
+                </Link>
+            </li>
+            <li className="nav-item">
                 <a className="nav-link">Agencies</a>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
                 <a className="nav-link">Missions</a>
             </li>
-            <Link to="/" className={window.location.pathname === "/"}>
-                <li class="nav-item">
+            <li className="nav-item">
+                <Link to="/">
                     <a className="nav-link" href="/">Home Base</a>
-                </li>
-            </Link>
+                </Link>
+            </li>
         </ul >
     );
 }
--- week-08/assessment/field-agent/src/components/pages/Agents.js
+++ week-08/assessment/field-agent/src/components/pages/Agents.js
@@ -1,16 +1,176 @@
 import "../../assets/css/Agents.css";
 import { useState, useEffect } from 'react';
+import Modal from 'react-modal';
 
 function Agents() {
     const [agents, setAgents] = useState([]);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [editAgentId, setEditAgentId] = useState(0);
+    const [firstName, setFirstName] = useState('');
+    const [middleName, setMiddleName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [dob, setDob] = useState('');
+    const [heightInInches, setHeightInInches] = useState(0);
+    const [errors, setErrors] = useState([]);
+    const [isAdd, setIsAdd] = useState(false);
+
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            width: "550px",
+            height: "450px",
+            fontSize: "24px",
+            marginTop: "20px",
+            backgroundColor: "lightslategray",
+            color: "white",
+            borderColor: "lightcoral",
+            borderRadius: "6px",
+            border: ".5px solid white",
+            padding: 5
+        }
+    };
+
+    function refreshPage() {
+		window.location.reload(false);
+	}
+
+    function closeModal() {
+        setModalIsOpen(false);
+    }
 
     useEffect(() => {
         fetch('http://localhost:8080/api/agent')
-          .then(response => response.json()) // assume that it's a 200
-          .then(data => setAgents(data))
-          .then(console.log('fetched data'))
-          .catch(error => console.log(error));
-      }, []);
+            .then(response => response.json()) // assume that it's a 200
+            .then(data => setAgents(data))
+            .then(console.log('fetched data'))
+            .catch(error => console.log(error));
+    }, []);
+
+    const handleAddSubmit = async (event) => {
+        event.preventDefault();
+        const newAgent = {
+            firstName,
+            middleName,
+            lastName,
+            dob,
+            heightInInches
+        };
+        const body = JSON.stringify(newAgent);
+
+        try {
+            // we need to make the fetch
+            const response = await fetch('http://localhost:8080/api/agent', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body
+            });
+
+            if (response.status === 200 || response.status === 400) {
+                const data = await response.json();
+                if (data.id) {
+                    console.log(...agents, data)
+                    setAgents([...agents, data]);
+                    setFirstName(' ');
+                    setMiddleName(' ');
+                    setLastName(' ');
+                    setDob(' ');
+                    setHeightInInches(' ');
+                    setErrors([]);
+                    setModalIsOpen(false);
+                    refreshPage();
+                } else {
+                    setModalIsOpen(false);
+                    setErrors(data);
+                }
+            } else {
+                console.log(response.status);
+                setModalIsOpen(false);
+                throw new Error('Error Error! Sorry:(');
+            }
+        } catch (error) {
+            setModalIsOpen(false);
+            console.log(error);
+        }
+    };
+
+
+    const handleUpdate = (id) => {
+        const agentToEdit = agents.find(agent => agent.agentId === id);
+
+        setEditAgentId(agentToEdit.agentId);
+        setFirstName(agentToEdit.firstName);
+        setMiddleName(agentToEdit.middleName);
+        setLastName(agentToEdit.lastName);
+        setDob(agentToEdit.dob);
+        setHeightInInches(agentToEdit.heightInInches);
+        setIsAdd(false);
+        setModalIsOpen(true)
+    };
+
+    const handleUpdateSubmit = async (event) => {
+        event.preventDefault();
+        const updatedAgent = {
+            agentId: editAgentId,
+            firstName,
+            middleName,
+            lastName,
+            dob,
+            heightInInches
+        };
+
+        const body = JSON.stringify(updatedAgent);
+
+        try {
+            // we need to make the fetch
+            const response = await fetch(`http://localhost:8080/api/agent/${editAgentId}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body
+            });
+
+            if (response.status === 200) {
+                const newAgents = [...agents];
+                const agentIndexToEdit = agents.findIndex(agent => agent.id === editAgentId);
+                newAgents[agentIndexToEdit] = {
+                    agentId: editAgentId,
+                    firstName,
+                    middleName,
+                    lastName,
+                    dob,
+                    heightInInches
+                };
+                setAgents(newAgents);
+                setFirstName(' ');
+                setMiddleName(' ');
+                setLastName(' ');
+                setDob(' ');
+                setHeightInInches(' ');
+                setErrors([]);
+                setModalIsOpen(false);
+                refreshPage();
+            } else if (response.status === 400) {
+                const data = await response.json();
+                setModalIsOpen(false);
+                setErrors(data);
+            } else {
+                setModalIsOpen(false);
+                console.log(response.status)
+                throw new Error('Error Error! Sorry:(');
+            }
+        } catch (error) {
+            setModalIsOpen(false);
+            console.log(error);
+        }
+    };
 
     return (
         <div className='container'>
@@ -18,63 +178,114 @@ function Agents() {
                 <h1 id='title' className="display-4">Agents</h1>
                 <p className="lead"></p>
             </div>
-            <table id='table' className="table table-dark">
-                <thead>
-                    <tr>
-                        <th scope="col">First</th>
-                        <th scope="col">Middle</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">DOB</th>
-                        <th scope="col">Height</th>
-                        <th scope="col">
-                        <button id='addButton' className="btn btn-primary btn-sm">
-                                {/* onClick={() => handleEdit(agent.id)}>Edit */}
-                                Add Agent
-                                </button>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                {agents.map(agent => (
-                    <tr key={agent.agentId}>
-                    <td>{agent.firstName}</td>
-                    <td>{agent.middleName}</td>
-                    <td>{agent.lastName}</td>
-                    <td>{agent.dob}</td>
-                    <td>{agent.heightInInches} inches</td>
-                    <td>
-                        <div className="float-right">
-                            <button id='editButton' className="btn btn-primary btn-sm">
-                                {/* onClick={() => handleEdit(agent.id)}>Edit */}
-                                Edit
-                                </button>
-                            <button id='deleteButton' className="btn btn-danger btn-sm ml-3">
-                                Delete
-                                {/* // onClick={() => handleDelete(agent.id)}>Delete */}
-                                </button>
+            <div id='table'>
+                <table className="table table-dark">
+                    <thead id='head'>
+                        <tr>
+                            <th scope="col">First</th>
+                            <th scope="col">Middle</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">DOB</th>
+                            <th scope="col">Height</th>
+                            <th scope="col">
+                                <button onClick={() => {
+                                    setModalIsOpen(true);
+                                    setIsAdd(true)
+                                }} id='addButton' className="btn btn-primary btn-sm">
+                                    Add Agent
+                            </button>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {agents.map(agent => (
+                            <tr key={agent.agentId}>
+                                <td>{agent.firstName}</td>
+                                <td>{agent.middleName}</td>
+                                <td>{agent.lastName}</td>
+                                <td> {agent.dob ? agent.dob : 'unknown'}</td>
+                                <td> {agent.heightInInches ? agent.heightInInches + ' inches' : 'unknown'}</td>
+                                <td>
+                                    <div className="float-right">
+                                        <button id='editButton' className="btn btn-primary btn-sm"
+                                            onClick={() => handleUpdate(agent.agentId)}>Edit
+                                    </button>
+                                        <button id='deleteButton' className="btn btn-danger btn-sm ml-3">
+                                            Delete
+                                        {/* // onClick={() => handleDelete(agent.id)}>Delete */}
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    ariaHideApp={false}
+                >
+                    <i id="close" className="fas fa-times" onClick={closeModal}></i>
+                    {isAdd ?
+                        <h1 className='modalTitle'>
+                            Add Agent
+                    </h1> :
+                        <h1 className='modalTitle'>
+                            Edit Agent
+                    </h1>}
+                    <form>
+                        <div className="form-group">
+                            {isAdd ?
+                                <input onChange={e => { setFirstName(e.target.value); console.log('first ' + e.target.value) }}
+                                    type="text" className="form-control" placeholder="First name" /> :
+                                <input onChange={e => { setFirstName(e.target.value); console.log('first ' + e.target.value) }}
+                                    type="text" className="form-control" defaultValue={firstName} />
+                            }
                         </div>
-                    </td>              
-                    </tr>
-                ))}
-
-                </tbody>
-            </table>
-            {/* <form>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-                </div>
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form> */}
+                        <div className="form-group">
+                            {isAdd ?
+                                <input onChange={e => { setMiddleName(e.target.value); console.log('middle ' + e.target.value) }}
+                                    type="text" className="form-control" placeholder="Middle name" /> :
+                                <input onChange={e => { setMiddleName(e.target.value); console.log('middle ' + e.target.value) }}
+                                    type="text" className="form-control" defaultValue={middleName} />
+                            }
+                        </div>
+                        <div className="form-group">
+                            {isAdd ?
+                                <input onChange={e => { setLastName(e.target.value); console.log('last ' + e.target.value) }}
+                                    type="text" className="form-control" placeholder="Last name" /> :
+                                <input onChange={e => { setLastName(e.target.value); console.log('last ' + e.target.value) }}
+                                    type="text" className="form-control" defaultValue={lastName} />
+                            }
+                        </div>
+                        <div className="form-group">
+                            {isAdd ?
+                                <input onChange={e => { setDob(e.target.value); console.log('dob ' + e.target.value) }}
+                                    type="date" className="form-control" placeholder="Date of birth" /> :
+                                <input onChange={e => { setDob(e.target.value); console.log('dob ' + e.target.value) }}
+                                    type="date" className="form-control" defaultValue={dob} />
+                            }
+                        </div>
+                        <div className="form-group">
+                            {isAdd ?
+                                <input onChange={e => { setHeightInInches(e.target.value); console.log('height ' + e.target.value) }}
+                                    type="number" className="form-control" placeholder="Height in inches" /> :
+                                <input onChange={e => { setHeightInInches(e.target.value); console.log('height ' + e.target.value) }}
+                                    type="number" className="form-control" defaultValue={heightInInches} />
+                            }
+                        </div>
+                        {
+                            isAdd ?
+                                <button onClick={handleAddSubmit} type="submit" className="btn btn-primary justify-content-center submitButton">Submit</button>
+                                :
+                                <button onClick={handleUpdateSubmit} type="submit" className="btn btn-primary justify-content-center submitButton">Submit</button>
+                        }
+                    </form>
+                </Modal>
+            </div>
         </div>
     )
 }
